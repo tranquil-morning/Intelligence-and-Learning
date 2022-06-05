a385a501072c7eea7ee3f64010d9b34a058f1df7
w0_0k <69587224+tranquil-morning@users.noreply.github.com>
2022-06-05 12:45:17 +0900

    Add Object Detection

---
+++ 02_Object Detection with COCO-SSD/sketch.js
@@ -0,0 +1,43 @@
+let video;
+let detector;
+let detections = [];
+
+preload = () => {
+  detector = ml5.objectDetector('cocossd');
+};
+
+gotDetections = (err, results) => {
+  if (err) {
+    console.log(err);
+  } else {
+    detections = results;
+    detector.detect(video,gotDetections);
+
+      
+    }
+  };
+
+setup = () => {
+  createCanvas(640, 480);
+  video = createCapture(VIDEO);
+  video.size(640, 480);
+  video.hide();
+  detector.detect(video, gotDetections); 
+};
+
+
+draw = () => {
+  image(video, 0, 0);
+
+  for (const e of detections) {
+    stroke(0, 255, 0);
+    strokeWeight(4);
+    noFill();
+    rect(e.x, e.y, e.width, e.height);
+    noStroke();
+    fill(255);
+    textSize(24);
+    text(e.label, e.x + 10, e.y + 24);
+  }
+
+};
